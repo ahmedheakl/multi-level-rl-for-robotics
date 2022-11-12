fc5830588ede277111b7f301f3688edd87494416
ahmedheakl <ahmed.heakl@ejust.edu.eg>
2022-11-12 20:24:08 +0200

    build workflows

--- src/highrl/lidar_setup/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-
-import numpy
-
-package = Extension(
-    "lidar2d_fast", ["lidar_setup/lidar2d_fast.pyx"], include_dirs=[numpy.get_include()]
-)
-setup(ext_modules=cythonize([package]))
