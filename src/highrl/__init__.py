ac23d01cdf80a79029ac65b23c85c026fb64ede4
ahmedheakl <ahmed.heakl@ejust.edu.eg>
2022-12-06 22:15:11 +0200

    readthedocs merge

--- src/highrl/__init__.py
+++ src/highrl/__init__.py
@@ -1,9 +1,5 @@
 # Version of highrl package
-<<<<<<< HEAD
 __version__ = "0.1.0"
 
 from highrl.utils.planner_checker import convex_hull_compute, convex_hull_difficulty, cross_product_point_line
 from highrl.__main__ import main
-=======
-__version__ = "0.1.0"
->>>>>>> d43a5220b17d8bbf27ddab5b46ca91440e4e615b
