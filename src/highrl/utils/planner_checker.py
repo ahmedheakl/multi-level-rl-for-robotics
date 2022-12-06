d5b9a256b41a70d6aafb1f5ba6ed610146eb4c26
ahmedheakl <ahmed.heakl@ejust.edu.eg>
2022-12-06 22:16:53 +0200

    readthedocs merge

--- src/highrl/utils/planner_checker.py
+++ src/highrl/utils/planner_checker.py
@@ -142,10 +142,6 @@ def check_valid_path_existance(
             if check_if_point_inside_polygen(p, coords):
                 x, y = p
                 env_map[x][y] = "X"
-<<<<<<< HEAD
-=======
-    # print(env_map)
->>>>>>> d43a5220b17d8bbf27ddab5b46ca91440e4e615b
     # bfs
     queue = [[px, py]]
     while len(queue) > 0:
@@ -211,13 +207,9 @@ def get_area_of_convex_polygen(points: List[List[int]]) -> float:
     return area
 
 def convex_hull_difficulty(obstacles: Obstacles, robot: Robot, width: int, height: int) -> Tuple[float, int]:
-<<<<<<< HEAD
     """Calculates env complexity using `convex_hull` algorithm
     
     Read more about `convex hull problem <https://en.wikipedia.org/wiki/Convex_hull>`_.
-=======
-    """Calculate env complexity using convex_hull algorithm
->>>>>>> d43a5220b17d8bbf27ddab5b46ca91440e4e615b
 
     Args:
         obstacles (Obstacles): env obstacles
@@ -228,10 +220,6 @@ def convex_hull_difficulty(obstacles: Obstacles, robot: Robot, width: int, heigh
     Returns:
         Tuple[float, int]: env area difficulty, env obstacles difficulty
     """
-<<<<<<< HEAD
-    
-=======
->>>>>>> d43a5220b17d8bbf27ddab5b46ca91440e4e615b
     px, py = robot.get_position()
     gx, gy = robot.get_goal_position()
     eps = 5
@@ -260,16 +248,4 @@ def convex_hull_difficulty(obstacles: Obstacles, robot: Robot, width: int, heigh
         if max_x >= width and max_y >= height and min_x <= 0 and min_y <= 0:
             break
     return INF, max(0, len(obstacles.obstacles_list) - 4)
-<<<<<<< HEAD
-
-def get_data(name: str = "ahmed") -> bool:
-    """
-    Handling user requests locally
-    
-    :param name: name of the user initiating the operation
-    :return: results of the request
-    """
-    return "why"
-=======
->>>>>>> d43a5220b17d8bbf27ddab5b46ca91440e4e615b
             
